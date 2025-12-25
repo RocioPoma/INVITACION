34ba1c09005a38b5823a648e96c9aab9408ec86b
RocioPomaSilvestre <pomarocio05@gmail.com>
2025-12-25 11:34:30 -0400

    primera version

--- src/app/components/sobre/sobre.component.ts
+++ src/app/components/sobre/sobre.component.ts
@@ -21,32 +21,8 @@ export class SobreComponent {
     this.isOpening = true;
     this.isOpen = true;
     
-    // Redirigir inmediatamente después de iniciar la animación
     setTimeout(() => {
       this.router.navigate(['/invitacion']);
-    }, 800); // Reducción del tiempo para una redirección más rápida
+    }, 800);
   }
 }
-// import { Component } from '@angular/core';
-// import { Router } from '@angular/router';
-// import { CommonModule } from '@angular/common';
-
-// @Component({
-//   selector: 'app-sobre',
-//   standalone: true,
-//   imports: [CommonModule],
-//   templateUrl: './sobre.component.html',
-//   styleUrls: ['./sobre.component.scss']
-// })
-// export class SobreComponent {
-//   isOpen = false;
-
-//   constructor(private router: Router) {}
-
-//   openInvitation() {
-//     this.isOpen = true;
-//     setTimeout(() => {
-//       this.router.navigate(['/invitacion']);
-//     }, 1500);
-//   }
-// }
